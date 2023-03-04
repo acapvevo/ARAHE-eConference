e81fba34f84657354e8cce2149c7595720b02e2f
Muhammad Nazirul Asyraaf Bin Halid <mn.asyraaf@graduate.utm.my>
2023-03-04 13:14:58 +0800

    add icon

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -1,4 +1,3 @@
 <a class="navbar-brand d-flex justify-content-center align-items-center sidebar-brand m-0" href="#">
-    <div class="sidebar-brand-icon rotate-n-15"><i class="fas fa-laugh-wink"></i></div>
-    <div class="sidebar-brand-text mx-3"><span>ARAHE</span></div>
+    <div class="sidebar-brand-icon"><img src="{{asset('assets/favicon/apple-touch-icon.png')}}" alt="ARAHE LOGO" height="70" width="70"></div>
 </a>
