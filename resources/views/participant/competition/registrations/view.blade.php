75a5f126017388f9b07c00f962796c781a4342dc
Muhammad Nazirul Asyraaf Bin Halid <mn.asyraaf@graduate.utm.my>
2023-03-05 13:13:36 +0800

    Update view.blade.php

--- resources/views/participant/competition/registrations/view.blade.php
+++ resources/views/participant/competition/registrations/view.blade.php
@@ -906,10 +906,12 @@
                                 $('.noExtraOption').css('opacity', '0.5');
                             } else {
 
+                                @if (!($currentChosenPackageFee->fullPackage ?? false))
                                 if ($('input:checkbox.extraFee').length) {
                                     $("input:checkbox.extraFee").prop('checked', false);
                                     $("input:checkbox.extraFee").attr("disabled", false);
                                 }
+                                @endif
 
                                 checkOption(category.fullPackage);
 
