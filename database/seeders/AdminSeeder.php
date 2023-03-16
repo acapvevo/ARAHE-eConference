9029afa57edcb355c3a9df7ce42f27f6efe00a52
Muhammad Nazirul Asyraaf Bin Halid <mn.asyraaf@graduate.utm.my>
2023-03-16 09:54:42 +0800

    Update AdminSeeder.php

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -22,5 +22,26 @@ class AdminSeeder extends Seeder
             'password' => Hash::make('admin1'),
             'email' => 'admin1@gmail.com'
         ]);
+
+        Admin::create([
+            'name' => 'Dr. Asimah',
+            'username' => 'admin2',
+            'password' => Hash::make('admin2'),
+            'email' => 'zsyimah@ftv.upsi.edu.my'
+        ]);
+
+        Admin::create([
+            'name' => 'Dr. Sarimah',
+            'username' => 'admin3',
+            'password' => Hash::make('admin3'),
+            'email' => 'isarimah1353@gmail.com'
+        ]);
+
+        Admin::create([
+            'name' => 'Dr. Suriani',
+            'username' => 'admin4',
+            'password' => Hash::make('admin4'),
+            'email' => 'sueforlife.mohamed@gmail.com'
+        ]);
     }
 }
