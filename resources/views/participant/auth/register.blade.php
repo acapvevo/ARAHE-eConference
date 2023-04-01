754903ceaabfdb0d23c10e7b0bad54dbba6c1ab5
Muhammad Nazirul Asyraaf Bin Halid <mn.asyraaf@graduate.utm.my>
2023-04-01 23:00:58 +0800

    modify pic in login and register page

--- resources/views/participant/auth/register.blade.php
+++ resources/views/participant/auth/register.blade.php
@@ -8,7 +8,8 @@
 @section('content')
     <div class="row">
         <div class="col-lg-6 d-none d-lg-flex">
-            <div class="flex-grow-1 bg-login-image" style="background-image: url(&quot;assets/img/dogs/image3.jpeg&quot;);">
+            <div class="flex-grow-1 bg-login-image"
+                style="background-image: url({{ asset('assets/img/background/bg.jpg') }});">
             </div>
         </div>
         <div class="col-lg-6">
